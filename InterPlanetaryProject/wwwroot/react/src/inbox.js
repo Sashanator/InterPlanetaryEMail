abb47983ae884e7e62fe7e3f55df07eda9edfe1f
Alexandr <ssorin99@mail.ru>
2021-08-11 14:13:05 +0300

    add frontend for table in inbox

--- InterPlanetaryProject/wwwroot/react/src/inbox.js
+++ InterPlanetaryProject/wwwroot/react/src/inbox.js
@@ -51,46 +51,27 @@ class Main extends Component {
     // TODO: Fix --> Return only main components!
     render() {
         return (
-            <div>
-              <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-                <ul className="navbar-nav px-3">
-                  <li className="nav-item text-nowrap d-none d-sm-none d-sm-block"> 
-                    <small className="text-white">{this.state.account}</small>
-                  </li>
-                </ul>
-              </nav>
-              <div className="container-fluid mt-5">
-              <main role="main" className="col-lg-12 d-flex text-center">
-                    <div className="content mr-auto ml-auto">
-                      <h2>Your letters</h2>
-                      <div className="form-group">
-                        <table className="table table-boarded">
+            <div className="table-users">
+              <div className="header">Letters</div>
+              <table cellspacing="0">
                           <tr>
-                            <th>ID</th>
-                            <th>From</th>
-                            <th>To</th>
                             <th>Theme</th>
                             <th>Text</th>
                             <th>IPFS</th>
+                            <th>From</th>
                           </tr>
                           {this.state.letters.map((letter) => (
                             <tr>
-                              <td>{letter[0]}</td>
-                              <td>{letter[1]}</td>
-                              <td>{letter[2]}</td>
                               <td>{letter[3]}</td>
                               <td>{letter[4]}</td>
                               {
                                 letter.length > 0 &&
                                 <td><ul>{letter[5].map((f) => <li><a href={`https://ipfs.infura.io/ipfs/${f}`}>FILE</a></li>)}</ul></td>
                               }
+                              <td className="cell expand-maximum-on-hover">{letter[1]}</td>
                             </tr>
                           ))}
                         </table>
-                      </div>
-                    </div>
-                  </main>
-              </div>
             </div>
           );
     }
